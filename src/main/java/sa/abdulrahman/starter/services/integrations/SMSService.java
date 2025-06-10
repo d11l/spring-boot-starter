7bd54c1fe96736a2ca34ff9284e908e2806a8a36
Abdulrahman radhi <abdulrahman.radhi.2002@gmail.com>
2025-06-10 05:48:23 +0300

    refactor: SMS service

--- src/main/java/sa/abdulrahman/starter/services/integrations/SMSService.java
+++ src/main/java/sa/abdulrahman/starter/services/integrations/SMSService.java
@@ -1,5 +1,6 @@
 package sa.abdulrahman.starter.services.integrations;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.scheduling.annotation.Async;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class SMSService {
 
     private final WebClient webClient;
@@ -22,32 +24,44 @@ public class SMSService {
     @Value("${msegat.sender.id}")
     private String senderId;
 
-    @Value("${msegat.api.url}")
-    private String apiUrl;
+    @Value("${msegat.sender.ad.id}")
+    private String senderAdId;
 
     public SMSService() {
         this.webClient = WebClient.builder()
-                .baseUrl(apiUrl)
+                .baseUrl("https://www.msegat.com/gw")
                 .build();
     }
 
+    public void sendOTP(String phone, String otp) {
+        send(senderId, phone, String.format("Pin Code is: %s", otp));
+    }
+
+    public void send(String phone, String message) {
+        send(senderId, phone, message);
+    }
+
+    public void sendAD(String phone, String message) {
+        send(senderAdId, phone, message);
+    }
+
     @Async
-    public void sendOTP(String phoneNumber, String otp) {
+    protected void send(String sender, String phone, String message) {
         try {
             webClient.post()
                     .uri("/sendsms.php")
                     .bodyValue(Map.of(
                             "apiKey", apiKey,
-                            "userSender", senderId,
                             "userName", username,
-                            "numbers", "966"+phoneNumber,
-                            "msg", String.format("Pin Code is: %s", otp)
+                            "userSender", sender,
+                            "numbers", "966"+phone,
+                            "msg", message
                     ))
                     .retrieve()
                     .toBodilessEntity()
                     .block();
         } catch (Exception e) {
-                throw new RuntimeException(e);
+            log.error("Failed to send SMS: {}", e.getMessage());
         }
     }
 
